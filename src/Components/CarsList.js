34a3cd4ecf1b417c5b58685381d02755fabaf046
janismozgis-rcs <janismozgis+rcs@gmail.com>
2022-02-14 20:54:47 +0200

    lesson 19

---
+++ src/Components/CarsList.js
@@ -0,0 +1,28 @@
+import getCars from "../Api/getCars";
+
+
+function CarsList() {
+    const cars = getCars()
+
+    const list = [];
+    for (const i in cars) {
+        const car = cars[i]
+        list.push(<li key={i}>
+            <strong>
+                {car}
+            </strong>
+        </li>)
+    }
+
+
+    return (
+        <div>
+            <h1>Simple cars</h1>
+            <ul>
+                {list}
+            </ul>
+        </div>
+    )
+}
+
+export default CarsList;
